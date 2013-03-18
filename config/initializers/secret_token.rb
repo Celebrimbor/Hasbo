c6cf5a43da85d649fd7b269657ebcfddea300a82
Kevin Tang <kevin@tinkerbox.com.sg>
2013-03-18 17:48:23 +0800

    Initial project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hasbo::Application.config.secret_token = '4010268643c71b05eb176dd5830c85b1bed8d9a3411d79010055748ebeee69be133fbeb66d5f4812737e612bcce7ae391ee4d3ecc51186d6421782b794f50105'
